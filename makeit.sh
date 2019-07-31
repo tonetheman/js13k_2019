30057f550810a1cb75e8b811b5e73be5d1192c24
Tony Colston <tonetheman@gmail.com>
2019-07-31 14:15:07 -0500

    first version

---
+++ makeit.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BUILD=build
+OUT=final.zip
+CL=closure-compiler-v20190709.jar
+
+echo "removing stuf..."
+rm -rf ${OUT}
+rm -rf ${BUILD}
+
+echo "making build dir..."
+mkdir ${BUILD}
+
+echo "copying files..."
+cp src/index.html ${BUILD}
+cp src/ga.js ${BUILD}
+
+echo "moving to build dir..."
+cd ${BUILD}
+
+echo "compressing ga..."
+java -jar ../${CL} --language_in=ECMASCRIPT5 --js ga.js  --js_output_file out.js
+
+echo "removing not used files..."
+rm -f ga.js
+
+echo "zipping dir..."
+zip ../${OUT} *
+
+cd ..
+
