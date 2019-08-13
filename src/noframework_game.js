53c2868166d9854c43d1e0a34c41c0639e7bc694
Tony Colston <tonetheman@gmail.com>
2019-08-13 13:57:53 -0500

    laying out ideas not sure yet

--- src/noframework_game.js
+++ src/noframework_game.js
@@ -1,5 +1,18 @@
 
 
+let G_GRAV = 4;
+
+class Player {
+    constructor() {
+        this.x = 0; //pos
+        this.y = 0;
+        this.dx = 0; // velocity
+        this.dy = 0;
+        this.ground = false;
+        this.jumpvel = 2; // not sure about this yet
+    }
+}
+
 class Sprite {
     constructor() {
         this.x = 0;
@@ -53,6 +66,65 @@ class GameLoading extends BaseState {
 class GamePlaying extends BaseState {
     constructor() {
         super();
+        this.player = new Player();
+    }
+    update(dt) {
+        // save our spot
+        let start_x = this.player.x;
+
+        // TODO: if we have a button press
+        // change vel now!
+        // if buttonPress && this.player.ground
+        // this.player.dy -= this.player.jumpvel
+
+        this.player.dx = 0;
+        // TODO if we have a left or right press
+        // if buttonPress left
+        // this.player.dx -= 2;
+        // if buttonPress right
+        // this.player.dx += 2;
+
+        // TODO:
+        // now move the player left/right
+        this.player.x += this.player.dx;
+
+        // TODO: check for walls? no clue
+        // maybe just move back to startx pos...
+
+        // add grav
+        this.player.dy += G_GRAV;
+
+        // now move in y direction
+        this.player.y += this.player.dy;
+
+        // TODO:
+        // need to check the tile under the player
+        // is it solid?
+
+        this.player.ground = false; // assume it is not
+
+        // only check if moving down
+        if (this.player.dy) > 0 {
+
+            // compute where you end up
+            // should i save y?
+
+            // stop moving down
+            this.player.dy = 0;
+        }
+
+        // TODO:
+        // check if tile above player is solid?
+
+        // only check if moving up
+        if (this.player.dy) < 0 {
+
+            // compute where you end up
+            // should i save y
+            
+            // stop moving up
+            this.player.dy = 0;
+        }
     }
 }
 class Game {
