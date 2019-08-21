dcfc78053b5a9c1eb4030ed58087abce91c0f33d
Tony Colston <tonetheman@gmail.com>
2019-08-21 13:23:01 -0500

    saving

--- src/testmini1.js
+++ src/testmini1.js
@@ -1,8 +1,53 @@
 
 
+let context = null;
+let W = 375, H = 667;
+let pixelsPerBlock = 8;
+
+// see here: 
+/*
+https://dev.opera.com/articles/3d-games-with-canvas-and-raycasting-part-1/
+*/
+
+let mapdata = [
+//[1,0,x,x,x,0,1],
+[1,1, 1,1,1, 1,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1],
+[1,0, 0,0,0, 0,1]
+];
+
+function drawMap() {
+    for(let i=0;i<mapdata.length;i++) {
+        for (let j=0;j<mapdata[0].length;j++) {
+            
+        }
+    }
+}
+
 function _init() {
     // c is the id of the canvas
     let tmp = kontra.init("c");
+    context = tmp.context;
+    canvas = tmp.canvas;
+
     // needed for mouse interaction
     kontra.initPointer();  
   }
@@ -14,7 +59,10 @@ function main() {
     let loop = kontra.GameLoop({ 
         update: function() { 
         },      
-        render: function() { 
+        render: function() {
+            context.fillRect(0,0,
+                canvas.width, canvas.height);
+            drawMap();
         }  
     });
     loop.start();    // start the game
